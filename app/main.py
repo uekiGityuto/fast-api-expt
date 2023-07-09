c5eb21ce7d97f98269143b27a99d9ef7a20b6c7e
Yuto Ueki <ueki.yuto.bz@gmail.com>
2023-07-09 02:38:58 +0000

    エラーハンドリング修正

--- app/main.py
+++ app/main.py
@@ -9,7 +9,6 @@ from sqlalchemy.orm import Session
 from app import crud, models, schemas
 from app.database import SessionLocal, engine
 from app.hash import verify_password
-from app.schemas import Token, TokenData, User
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
@@ -21,6 +20,13 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Error Messages
+ERROR_LOGIN_FAILURE = "LOGIN_FAILURE"
+ERROR_INVALID_CREDENTIALS = "INVALID_CREDENTIALS"
+ERROR_INACTIVE_USER = "INACTIVE_USER"
+ERROR_USER_ALREADY_EXISTS = "USER_ALREADY_EXISTS"
+ERROR_USER_NOT_FOUND = "USER_NOT_FOUND"
+
 
 # Dependency
 def get_db():
@@ -31,19 +37,17 @@ def get_db():
         db.close()
 
 
-def get_user(db, email: str) -> User:
-    db_user = crud.get_user_by_email(db, email=email)
-    if db_user is None:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="LOGIN_FAILURE")
-    return db_user
+def _raise_http_exception(status_code: int, detail: str):
+    raise HTTPException(status_code=status_code, detail=detail)
 
 
-def authenticate_user(db: Session, email: str, password: str) -> User:
+def authenticate_user(db: Session, email: str, password: str) -> models.User:
     user = crud.get_user_by_email(db, email=email)
     if user is None:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="LOGIN_FAILURE")
+        _raise_http_exception(status.HTTP_403_FORBIDDEN, ERROR_LOGIN_FAILURE)
+    assert user is not None
     if not verify_password(password, user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="LOGIN_FAILURE")
+        _raise_http_exception(status.HTTP_403_FORBIDDEN, ERROR_LOGIN_FAILURE)
     return user
 
 
@@ -58,10 +62,10 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
     return encoded_jwt
 
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)) -> schemas.User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
+        detail=ERROR_INVALID_CREDENTIALS,
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
@@ -69,40 +73,30 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         email: str = payload.get("sub")  # type: ignore
         if email is None:
             raise credentials_exception
-        token_data = TokenData(email=email)
     except JWTError:
         raise credentials_exception
-    user = get_user(db, email=token_data.email)
+    user = crud.get_user_by_email(db, email=email)
     if user is None:
         raise credentials_exception
     return user
 
 
-def get_current_active_user(current_user: User = Depends(get_current_user)) -> User:
+def get_current_active_user(current_user: schemas.User = Depends(get_current_user)) -> schemas.User:
     if not current_user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
+        _raise_http_exception(status.HTTP_400_BAD_REQUEST, ERROR_INACTIVE_USER)
     return current_user
 
 
-@app.post("/token", response_model=Token)
+@app.post("/token", response_model=schemas.Token)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(
-        db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    user = authenticate_user(db, form_data.username, form_data.password)
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.email}, expires_delta=access_token_expires
-    )
+    access_token = create_access_token(data={"sub": user.email}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@app.get("/users/me/", response_model=User)
-def read_users_me(current_user: User = Depends(get_current_active_user)):
+@app.get("/users/me/", response_model=schemas.User)
+def read_users_me(current_user: models.User = Depends(get_current_active_user)):
     return current_user
 
 
@@ -110,7 +104,7 @@ def read_users_me(current_user: User = Depends(get_current_active_user)):
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+        _raise_http_exception(status.HTTP_400_BAD_REQUEST, ERROR_USER_ALREADY_EXISTS)
     return crud.create_user(db=db, user=user)
 
 
@@ -124,14 +118,12 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        _raise_http_exception(status.HTTP_404_NOT_FOUND, ERROR_USER_NOT_FOUND)
     return db_user
 
 
 @app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
+def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
