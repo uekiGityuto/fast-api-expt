151f403d5f9d2ad586acb0f5f2761d9c6470bf19
Yuto Ueki <ueki.yuto.bz@gmail.com>
2023-07-12 12:28:07 +0000

    ファイル名変更

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
-from app.api import admin, login, users
+from app.api import admin, login, user
 
 app = FastAPI()
 
-app.include_router(users.router)
+app.include_router(user.router)
 app.include_router(login.router)
 app.include_router(admin.router)
